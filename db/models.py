a056dc037b1671a9668279d4de4808045fe53bc7
Liubomyr Levkovych <liubomyrlevkovich@gmail.com>
2025-03-27 17:51:43 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -94,21 +94,21 @@ class Ticket(models.Model):
         return f"{self.movie_session} (row: {self.row}, seat: {self.seat})"
 
     def clean(self) -> None:
-        rws = self.movie_session.cinema_hall.rows
-        if not (1 <= self.row <= rws):
-            raise ValidationError({"row":
-                                       f"row number must be in available range: "
-                                       f"(1, rows): "
-                                       f"(1, "
-                                       f"{rws})"})
-
-        sts = self.movie_session.cinema_hall.seats_in_row
-        if not (1 <= self.seat <= sts):
-            raise ValidationError({"seat":
-                                       f"seat number must be in available range: "
-                                       f"(1, seats_in_row): "
-                                       f"(1, "
-                                       f"{sts})"})
+        if not (1 <= self.row <= self.movie_session.cinema_hall.rows):
+            raise ValidationError({
+                "row": [
+                    f"row number must be in available range: "
+                    f"(1, rows): (1, {self.movie_session.cinema_hall.rows})"
+                ]
+            })
+        if not (1 <= self.seat <= self.movie_session.cinema_hall.seats_in_row):
+            raise ValidationError({
+                "seat": [
+                    f"seat number must be in available range: "
+                    f"(1, seats_in_row):"
+                    f" (1, {self.movie_session.cinema_hall.seats_in_row})"
+                ]
+            })
 
     def save(self, *args, **kwargs) -> None:
         self.full_clean()
